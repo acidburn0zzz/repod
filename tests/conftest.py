1df2adb6a48fc50c5ff711093264f7da6757ab76
David Runge <dvzrv@archlinux.org>
2022-05-12 01:06:42 +0200

    Add module to parse mtree files
    repod/files/mtree.py:
    Add a module to read and validate `.MTREE` files (found in libalpm
    compatible package files).
    Add versioned schema for entries in an `.MTREE` file, which is based on
    a subset of the known `mtree` keywords.
    
    tests/conftest.py:
    Add fixtures for text streams representing the content of an .MTREE file
    as displayed by bsdcat.
    
    tests/files/test_mtree.py:
    Add tests for mtree functionality.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
+import gzip
+from io import BytesIO, StringIO
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import IO, Generator
+
 from pydantic import BaseModel
+from pytest import fixture
 
 from repod.models import Files, OutputPackageBase, PackageDesc
 
@@ -17,3 +24,68 @@ class OutputPackageBaseV9999(OutputPackageBase, SchemaVersion9999):
 
 class PackageDescV9999(PackageDesc, SchemaVersion9999):
     pass
+
+
+@fixture(scope="function")
+def mtreeentryv1_stringio() -> Generator[StringIO, None, None]:
+    mtree_contents = (
+        "#mtree\n"
+        "/set type=file uid=0 gid=0 mode=644\n"
+        "./.BUILDINFO time=1651787473.0 size=5651 md5digest=f712adf35b8a74755b3a93997b05793c "
+        "sha256digest=ed4e5855da200753eaf00cd584f017bef6910c09f70d72e4a642515312919804\n"
+        "./.INSTALL time=1651787473.0 size=808 md5digest=d96cc20315471e332a06c4261590b505 "
+        "sha256digest=96fc0c8b3aa4011de41c9dd1ba95e63bf2e9767daa801c14bea1d57359baf307\n"
+        "./.PKGINFO time=1651787473.0 size=1689 md5digest=76ff63a0094096fabe790fb35daadf79 "
+        "sha256digest=e15a57a4ddb0fa9feddbe410f395524b25be345d17f75f5f2ccc273034d388bc\n"
+        "/set mode=755\n"
+        "./etc time=1651787473.0 type=dir\n"
+        "./etc/uwsgi time=1651787473.0 type=dir\n"
+        "./etc/uwsgi/nextcloud.ini time=1651787473.0 mode=644 size=2761 "
+        "md5digest=c6e1c562468738e93335f2e2ce314e8b "
+        "sha256digest=87d2b2075fbb24eb1108fed7ef9f2971d7954ae0894b1405425a04ff9e1df49e\n"
+        "./etc/webapps time=1651787473.0 type=dir\n"
+        "./usr/share/webapps/nextcloud/.htaccess time=1651787473.0 mode=777 type=link "
+        "link=/etc/webapps/nextcloud/.htaccess\n"
+    )
+
+    yield StringIO(initial_value=mtree_contents)
+
+
+@fixture(scope="function")
+def invalid_mtreeentryv1_stringio() -> Generator[StringIO, None, None]:
+    mtree_contents = (
+        "#mtree\n"
+        "/set type=file uid=2000 gid=0 mode=644\n"
+        "./.BUILDINFO time=1651787473.0 size=5651 md5digest=f712adf35b8a74755b3a93997b05793c "
+        "sha256digest=ed4e5855da200753eaf00cd584f017bef6910c09f70d72e4a642515312919804\n"
+        "./.INSTALL time=1651787473.0 size=808 md5digest=d96cc20315471e332a06c4261590b505 "
+        "sha256digest=96fc0c8b3aa4011de41c9dd1ba95e63bf2e9767daa801c14bea1d57359baf307\n"
+        "./.PKGINFO time=1651787473.0 size=1689 md5digest=76ff63a0094096fabe790fb35daadf79 "
+        "sha256digest=e15a57a4ddb0fa9feddbe410f395524b25be345d17f75f5f2ccc273034d388bc\n"
+        "/set mode=755\n"
+        "./etc time=1651787473.0 type=dir\n"
+        "./etc/uwsgi time=1651787473.0 type=dir\n"
+        "./etc/uwsgi/nextcloud.ini time=1651787473.0 mode=644 size=2761 "
+        "md5digest=c6e1c562468738e93335f2e2ce314e8b "
+        "sha256digest=87d2b2075fbb24eb1108fed7ef9f2971d7954ae0894b1405425a04ff9e1df49e\n"
+        "./etc/webapps time=1651787473.0 type=dir\n"
+        "./usr/share/webapps/nextcloud/.htaccess time=1651787473.0 mode=777 type=link "
+        "link=/etc/webapps/nextcloud/.htaccess\n"
+    )
+
+    yield StringIO(initial_value=mtree_contents)
+
+
+@fixture(scope="function")
+def valid_mtree_file(mtreeentryv1_stringio: StringIO) -> Generator[Path, None, None]:
+    with NamedTemporaryFile() as mtree_file:
+        with gzip.open(filename=mtree_file.name, mode="wt") as gzip_mtree:
+            gzip_mtree.write(mtreeentryv1_stringio.getvalue())
+
+        yield Path(mtree_file.name)
+
+
+@fixture(scope="function")
+def valid_mtree_bytesio(valid_mtree_file: Path) -> Generator[IO[bytes], None, None]:
+    with open(valid_mtree_file, mode="rb") as gzip_mtree:
+        yield BytesIO(gzip_mtree.read())
